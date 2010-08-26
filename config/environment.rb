93a712cbf61fb7147771afc0d4635e9af3a4f611
Chris Polis <cmpolis@gmail.com>
2010-08-26 15:29:23 -0700

    clearance (authentication) installed

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,10 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+
+  # CLEARANCE
+  config.gem 'clearance'  
+
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,8 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
+# CLEARANCE
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 # The test environment is used exclusively to run your application's
 # test suite.  You never need to work with it otherwise.  Remember that
 # your test database is "scratch space" for the test suite and is wiped
