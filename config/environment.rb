87bddd239f6901d66deb39d0ff5c6acd5659f266
Chris Polis <cmpolis@gmail.com>
2010-08-27 12:17:01 -0700

    messing around w/ performance testing...

--- config/environment.rb
+++ config/environment.rb
@@ -11,6 +11,9 @@ Rails::Initializer.run do |config|
   # CLEARANCE
   config.gem 'clearance'  
 
+  # Performance Testing
+  # config.gem 'ruby-prof'
+
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
