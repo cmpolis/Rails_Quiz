44c868e278e697f2deb6bf40fd0b89ccfc24d376
Chris Polis <cmpolis@gmail.com>
2010-08-27 01:47:29 -0700

    added tests, controller code to ensure logged in for quiz#new

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,5 +1,10 @@
 class QuizzesController < ApplicationController
   def new
+    if current_user.nil?
+      flash[:notice] = "Must be logged in to make a quiz"
+      redirect_to root_url
+    else
+    end
   end
 
   def show
--- test/functional/quizzes_controller_test.rb
+++ test/functional/quizzes_controller_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class QuizzesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "If not logged in, make quiz redirects to root" do
+    @controller.current_user = nil
+    get :new
+    assert_redirected_to root_url
+    assert_equal "Must be logged in to make a quiz", flash[:notice]
   end
+
+  test "If logged in, renders new quiz form" do
+    @controller.current_user = Factory(:user)
+    get :new
+    assert_template :new
+  end
+
 end
