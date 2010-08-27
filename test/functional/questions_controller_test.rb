73701b258149f6bdf915a2c74e47b4cd7572b25a
Chris Polis <cmpolis@gmail.com>
2010-08-27 15:11:03 -0700

    added test for question#new

--- test/functional/questions_controller_test.rb
+++ test/functional/questions_controller_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "Must be quiz creator for question#new" do
+    @controller.current_user = nil
+    get :new, :id => 1
+    assert_redirected_to root_url
+    assert_equal "Must be quiz creator to add a question", flash[:notice]
+    @controller.current_user = 2384
+    get :new, :id => 1
+    assert_redirected_to root_url
+    assert_equal "Must be quiz creator to add a question", flash[:notice]
+
+    @controller.current_user = Factory(:user)
+    quiz = quizzes(:valid)
+    quiz.creator_id = @controller.current_user
+    quiz.save
+    get :new, :id => quiz.id
+    assert_template :new
   end
+
 end
