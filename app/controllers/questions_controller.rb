0abef4b95a891fa3d1476e7a0b0b65058fcfe616
Chris Polis <cmpolis@gmail.com>
2010-08-27 15:30:52 -0700

    working on tests, controller for question#new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,15 @@
 class QuestionsController < ApplicationController
+  
   def new
+    if Quiz.find(params[:id]).nil?
+      redirect_to root_url
+      flash[:notice] = "Invalid quiz id"
+    elsif !current_user.nil? or Quiz.find(params[:id]).creator_id != current_user.id
+      redirect_to root_url
+      flash[:notice] = "Must be quiz creator to add a question"
+    else
+      render :new
+    end
   end
 
   def show
--- test/functional/questions_controller_test.rb
+++ test/functional/questions_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionController::TestCase
-  
+=begin
+  test "Redirect to root if question#new gives invalid id" do
+    get :new, :id => nil
+    assert_redirected_to root_url
+    assert_equal "Invalid quiz id"
+
+    get :new, :id => 12345
+    assert_redirected_to root_url
+    assert_equal "Invalid quiz id" 
+  end  
+=end
   test "Must be quiz creator for question#new" do
     @controller.current_user = nil
     get :new, :id => 1
@@ -11,9 +21,10 @@ class QuestionsControllerTest < ActionController::TestCase
     get :new, :id => 1
     assert_redirected_to root_url
     assert_equal "Must be quiz creator to add a question", flash[:notice]
-
-    @controller.current_user = Factory(:user)
-    quiz = quizzes(:valid)
+  
+    user = Factory(:user)
+    @controller.current_user = user
+    quiz = quizzes(:valid)  
     quiz.creator_id = @controller.current_user
     quiz.save
     get :new, :id => quiz.id
