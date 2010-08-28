b689d93c2d542d85973a5f6faa656c7ea96da047
Chris Polis <cmpolis@gmail.com>
2010-08-28 16:30:10 -0700

    created basic user profile view, users#show

--- app/models/quiz_entry.rb
+++ app/models/quiz_entry.rb
@@ -8,4 +8,14 @@ class QuizEntry < ActiveRecord::Base
   has_many :answers, :through => :answer_entries
 
   validates_presence_of :user_id, :quiz_id
+ 
+  # Returns number of correct answers
+  def correct
+    (answers.find_all_by_right(true)).length
+  end
+   
+  def possible
+    quiz.questions.length
+  end
+
 end
