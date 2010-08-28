abfb24a32b20586069eee0aa68a4fd46c0f9c9b7
Chris Polis <cmpolis@gmail.com>
2010-08-28 15:00:44 -0700

    basic take quiz functionality done

--- app/controllers/quiz_entries_controller.rb
+++ app/controllers/quiz_entries_controller.rb
@@ -1,12 +1,39 @@
 class QuizEntriesController < ApplicationController
   def new
-    @quiz = Quiz.find(params[:id])
-    @questions = @quiz.questions # Filter here as neccesary
-    @quiz_entry = QuizEntry.new
+    if current_user.nil? 
+      flash[:notice] = "Must be logged in to take quiz"
+      redirect_to root_url
+    else
+      @quiz = Quiz.find(params[:id])
+      @questions = @quiz.questions # Filter here as neccesary
+      @quiz_entry = QuizEntry.new
+      @user = current_user
+    end
   end
 
   def create
-        
+    @quiz_entry = QuizEntry.new params[:quiz_entry]
+    
+    # QEntry needs an ID for answer_entries to have an ID pass validations
+    #   save QEntry first (w/o answers), then add answers, validate answers
+    @quiz_entry.save if @quiz_entry.valid?
+    params[:answers].each do |answer|
+      @quiz_entry.answer_entries.build :answer_id => answer
+    end
+    # check for:
+    #   multiple selections on one question
+    #   no answer for a question
+    #   etc...
+ 
+    if @quiz_entry.save
+      redirect_to :controller => "quiz_entries", :action => "show", :id => @quiz_entry.id
+    else
+      flash[:notice] = "Quiz submission unsuccesful"
+      @user = current_user
+      @quiz = @quiz_entry.quiz
+      @questions = @quiz.questions
+      render :new
+    end
   end
 
   def show
--- app/models/quiz_entry.rb
+++ app/models/quiz_entry.rb
@@ -2,7 +2,7 @@ class QuizEntry < ActiveRecord::Base
   belongs_to :user
   belongs_to :quiz
 
-  has_many :answer_entries, :dependent => :destroy
+  has_many :answer_entries, :dependent => :destroy, :autosave => true
   accepts_nested_attributes_for :answer_entries
 
   validates_presence_of :user_id, :quiz_id
