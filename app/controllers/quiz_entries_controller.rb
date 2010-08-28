acf02b2b9151aae2db488706f473ebdd3d984e05
Chris Polis <cmpolis@gmail.com>
2010-08-28 14:18:58 -0700

    started implementation of taking a quiz (quizentry#new)

--- app/controllers/quiz_entries_controller.rb
+++ app/controllers/quiz_entries_controller.rb
@@ -1,5 +1,12 @@
 class QuizEntriesController < ApplicationController
   def new
+    @quiz = Quiz.find(params[:id])
+    @questions = @quiz.questions # Filter here as neccesary
+    @quiz_entry = QuizEntry.new
+  end
+
+  def create
+        
   end
 
   def show
--- app/models/quiz_entry.rb
+++ app/models/quiz_entry.rb
@@ -2,5 +2,8 @@ class QuizEntry < ActiveRecord::Base
   belongs_to :user
   belongs_to :quiz
 
+  has_many :answer_entries, :dependent => :destroy
+  accepts_nested_attributes_for :answer_entries
+
   validates_presence_of :user_id, :quiz_id
 end
