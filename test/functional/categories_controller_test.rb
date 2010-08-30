8731ae7397f3d7521eff12dad73650b76453e1ff
Chris Polis <cmpolis@gmail.com>
2010-08-30 14:43:11 -0700

    categories#show is functional

--- test/functional/categories_controller_test.rb
+++ test/functional/categories_controller_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "categories#show should redirect if invalid id is given" do
+    get :show, { :id => nil }
+    assert_redirected_to root_url
+    get :show, { :id => 82034 }
+    assert_redirected_to root_url
+  end
+
+  test "categories#show should render show if id is valid" do
+    cat = Factory(:category)
+    get :show, { :id => cat.id }
+    assert_template :show
   end
 end
