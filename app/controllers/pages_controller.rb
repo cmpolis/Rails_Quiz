540136dd62b65d5e5000961b638c3ed71b6cfc38
Chris Polis <cmpolis@gmail.com>
2010-08-30 18:47:37 -0700

    created basic admin page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,17 @@ class PagesController < ApplicationController
   def about
   end
 
+  def admin
+    
+    # basic admin authentication for now...
+    if current_user.id == 1
+      @quizzes = Quiz.all
+      @users = User.all
+      @groups = Group.all
+    else
+      flash[:notice] = "You do not have admin privelages"
+      redirect_to root_url
+    end
+  end
+
 end
