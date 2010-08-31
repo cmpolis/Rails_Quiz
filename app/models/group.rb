20cd8fa8af1e30da393317d6a3b025683065fbb8
Chris Polis <cmpolis@gmail.com>
2010-08-30 20:08:57 -0700

    fixed Group.users association (class name was lowercase)

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
   
-  has_one :admin, :class_name => "user"
+  has_one :admin, :class_name => "User"
   has_many :group_memberships
   has_many :users, :through => :group_memberships
   has_many :quizzes
