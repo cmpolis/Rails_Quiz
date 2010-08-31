5e1220abefc919974116b0509fc37b638c1a6609
Chris Polis <cmpolis@gmail.com>
2010-08-30 18:47:27 -0700

    capitalize class names for associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
 
   has_many :group_memberships
   has_many :groups, :through => :group_memberships
-  has_many :admin_groups, :class_name => "group", :foreign_key => "admin_id"
-  has_many :created_quizzes, :class_name => "quiz", :foreign_key => "creator_id"
+  has_many :admin_groups, :class_name => "Group", :foreign_key => "admin_id"
+  has_many :created_quizzes, :class_name => "Quiz", :foreign_key => "creator_id"
   has_many :quiz_entries
   has_many :quizzes, :through => :quiz_entries
 
