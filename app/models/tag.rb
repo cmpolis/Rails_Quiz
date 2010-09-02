33b94a5d7e2a3363ab62986c6373f546562fd4d1
Chris Polis <cmpolis@gmail.com>
2010-09-02 03:30:00 -0700

    wrote Tag.parse method

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,5 +5,16 @@ class Tag < ActiveRecord::Base
 
   validates_length_of :text, :in => 3..20
 
+  # Gets a string formatted "tag1, tag2, tag3" and an id for a quiz
+  # Creates tags if they are not yet in the database
+  # Creates tagging for each tag to link with quiz
+  def self.parse quiz, tag_string
+    tags = tag_string.downcase.split /\s*,\s*/
+    tags.each do |tag|
+      tag_obj = Tag.find_by_text(tag)
+      tag_obj = Tag.create(:text => tag) if tag_obj.nil?
+      tag_obj.taggings.create :quiz_id => quiz
+    end
+  end
 
 end
--- test/unit/tag_test.rb
+++ test/unit/tag_test.rb
@@ -24,4 +24,20 @@ class TagTest < ActiveSupport::TestCase
     assert tag.respond_to? "taggings"
   end
 
+  # in models/tag.rb: def self.parse quiz, tag_string
+  test "Tag.prase creates new tags if they are not yet in db" do
+    tags = "fishing, hiking, swimming"
+    assert_difference(['Tag.count', 'Tagging.count'], +3) do
+      Tag.parse 1, tags
+    end
+  end
+
+  test "Tag.parse does not create a new tag if it already exists" do
+    Tag.parse 1, "music, literature, math"
+    assert_no_difference 'Tag.count' do
+      Tag.parse 23, "music, math"
+    end
+    assert_equal 2, Tagging.find_all_by_quiz_id(23).length 
+  end
+
 end
