c89369eb0a88539124cdd9d8a16580f6f7ff8205
Chris Polis <cmpolis@gmail.com>
2010-09-08 19:06:26 -0700

    create/new updated for multiple choice, short answer and list questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,26 +15,6 @@ class QuestionsController < ApplicationController
   end
 
   def create
-=begin
-NOTE: moved question validation logic to each quiz type class
-      allows for simpler controller logic and multiple question types
-
-@question = Question.new(params[:question])  
-    @question.valid?  # generate errors for questions
-
-    right_answers = @question.answers.find_all { |ans| ans.right }    
-    @question.errors.add_to_base "Must have exactly one right answer" \
-           if right_answers.length != 1
-    
-    if @question.errors.empty?
-      @question.save
-      flash[:notice] = "Question added"
-      redirect_to add_question_path(@question.quiz)
-    else
-      @quiz = @question.quiz
-      render :new
-    end
-=end
     @quiz = Quiz.find(params[:question][:quiz_id])
     @question = @quiz.validate_question params
     if @question.errors.empty?
--- app/models/short_answer.rb
+++ app/models/short_answer.rb
@@ -1,14 +1,16 @@
 class ShortAnswer < Quiz
 
   def build_question
-    question = (1..4).collect { self.questions.build }
-    question.each {|q| q.answers.build }
+    question = self.questions.build
+    question.answers.build
     question
   end
 
   # Tries to save question(s) from params, returns true if saved
   def validate_question params
-    
+    @question = Question.new(params[:question])
+    @question.valid?  # generate errors for question
+    @question
   end
 
 end
